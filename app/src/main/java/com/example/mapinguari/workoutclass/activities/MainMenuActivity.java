507906a5167a0f9dfa1a64bd25c645488b833bad
unknown <zincdemon@users.noreply.github.com>
2015-08-16 18:09:22 +0100

    Signed-off-by: unknown <zincdemon@users.noreply.github.com>

--- app/src/main/java/com/example/mapinguari/workoutclass/activities/MainMenuActivity.java
+++ app/src/main/java/com/example/mapinguari/workoutclass/activities/MainMenuActivity.java
@@ -3,14 +3,23 @@ package com.example.mapinguari.workoutclass.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.net.Uri;
+import android.provider.MediaStore;
 
 import com.example.mapinguari.workoutclass.R;
+import com.example.mapinguari.workoutclass.ImgProcess;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Vector;
 
 
 public class MainMenuActivity extends ActionBarActivity {
+    static final int GET_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,38 @@ public class MainMenuActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_menu, menu);
         return true;
+    }
+
+    public void LoadImage (View v){
+        Intent LoadImageIntent = new Intent(Intent.ACTION_GET_CONTENT );
+        LoadImageIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        LoadImageIntent.setType("image/*");
+        startActivityForResult(LoadImageIntent, GET_IMAGE_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case GET_IMAGE_REQUEST:
+                if (resultCode==RESULT_OK){
+                    Uri imageUri=data.getData();
+                    Vector<Vector<String>> ocrReturnedValues;
+
+                    ImgProcess.loadLanguage("lan",this.getApplicationContext());
+
+                    try {
+                        ocrReturnedValues=ImgProcess.ProcessImage(
+                                MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri),
+                                this.getCacheDir().getCanonicalPath());
+                    } catch (Exception e) {
+                        Log.e("LoadImage", e.toString());
+                        e.printStackTrace();
+                    }
+                    //do something with returned values here
+                }
+
+            default:
+        }
+
     }
 
     @Override
