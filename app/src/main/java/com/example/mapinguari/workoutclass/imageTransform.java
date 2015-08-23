60a2acffaee16cad23b3ad8647b510940554b224
zincdemon <zincdemon@users.noreply.github.com>
2015-08-23 20:51:24 +0100

    Skeleton for area transformation class

---
+++ app/src/main/java/com/example/mapinguari/workoutclass/imageTransform.java
@@ -0,0 +1,12 @@
+package com.example.mapinguari.workoutclass;
+
+import android.graphics.Bitmap;
+
+class imageTransform {
+    public static Bitmap TransformAreaToSquare(Bitmap imageIN,
+                                         int xTopLeft, int xTopRight, int xBottomLeft, int xBottomRight,
+                                         int yTopLeft, int yTopRight, int yBottomLeft, int yBottomRight) {
+
+        return imageIN;
+    }
+}
